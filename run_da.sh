766fe6d51e733b9afa1bc8cd6c03394748262b93
Piotr Żelasko <petezor@gmail.com>
2020-05-21 22:22:23 -0400

    Data prep sanity

--- run_da.sh
+++ run_da.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
 # My own setup for the CLSP grid - you might need to adjust it for your own purposes
+
+if [ $# -ne 1]; then
+  echo "Usage: run_da.sh <experiment-directory>"
+  exit 1
+fi
+
+EXP_DIR="$(readlink -f "$1")"
+mkdir -p $EXP_DIR
+cd $EXP_DIR
+
 if [[ $(hostname -f) == *.clsp.jhu.edu ]]; then
-  cd /home/pzelasko/daseg/deps/transformers/examples/ner
   source /home/pzelasko/miniconda3/bin/activate
   conda activate swda
   num_gpus=1
   CUDA_VISIBLE_DEVICES=$(free-gpu -n $num_gpus)
   export CUDA_VISIBLE_DEVICES
-else
-  cd deps/transformers/examples/ner
 fi
 
 set -eou pipefail
 
-export OUTPUT_DIR=swda-xlmroberta-t45-nospacy-tb
-export BERT_MODEL=xlm-roberta-base
-export MODEL_TYPE=xlmroberta
+export OUTPUT_DIR=xlnet-t46
+export BERT_MODEL=xlnet-base-cased
+export MODEL_TYPE=xlnet
 export MAX_LENGTH=512
+
+dasg prepare-data \
+  --strip-punctuation-and-lowercase \
+  --continuations-allowed \
+  --swda-path ../deps/swda/swda \
+  ./
 
 wget "https://raw.githubusercontent.com/stefan-it/fine-tuned-berts-seq/master/scripts/preprocess.py"
 python3 preprocess.py train.txt.tmp $BERT_MODEL $MAX_LENGTH > train.txt
