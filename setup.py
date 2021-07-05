a3c2d1e356f5f96ed29c1240fa68d239127be990
Piotr Żelasko <petezor@gmail.com>
2021-07-05 15:24:13 -0400

    Add Apache 2.0 license

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 setup(
     name='daseg',
+    license='Apache-2.0 License',
     version='0.1',
     packages=find_packages(),
     scripts=['daseg/bin/dasg']
